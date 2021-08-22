e30da8aa7752dcf63d3ba1385a8d8440a8d776cc
Tushar Akhade <tusharakhade10@gmail.com>
2021-08-23 03:14:22 +0530

    Added: inner html using template literals in a script from a js file to display employee payroll details in tabular format

---
+++ js/employee_home_script.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", () => {
+    createInnerHtml();
+});
+
+const createInnerHtml = () => {
+    const headerHtml =
+        "<th></th>" +
+        "<th>Name</th>" +
+        "<th>Gender</th>" +
+        "<th>Department</th>" +
+        "<th>Salary</th>" +
+        "<th>Start Date</th>" +
+        "<th>Actions</th>";
+    const innerHtml = `${headerHtml}
+    <tr>
+        <td><img class="profile" alt="" src="..//assets/profile-images/Ellipse -2.png"></td>
+        <td>Aditya Verma</td>
+        <td>Male</td>
+        <td>
+            <div class="dept-label">HR</div>
+            <div class="dept-label">Finance</div>
+        </td>
+        <td>300000</td>
+        <td>1 Jan 2020</td>
+        <td>
+            <img id="1" onclick="remove(this)" alt="delete" src="..//assets/icons/delete-black-18dp.svg">
+            <img id="1" onclick="update(this)" alt="edit" src="..//assets/icons/create-black-18dp.svg">
+        </td>
+    </tr>
+    `;
+    document.querySelector("#display").innerHTML = innerHtml;
+};
